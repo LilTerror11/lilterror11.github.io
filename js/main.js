7f9edea31053f0ae24696098bbb4e122ee2f613a
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-03-13 13:15:58 +0930

    um i duno

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function alert_me() {
     alert("hello there!")
 }
+
+cookies = document.cookie
+if (cookies=="") {
+    document.cookie
+}
+
+count = document.cookie.valueOf("count")
+
+function get_variable_value() {
+    alert("repository variable: $(count)")
+}
